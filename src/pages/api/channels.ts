2db79eee0b72e226533021a2a5ac1fa70fd51f55
Simon <sn@hitbox.tv>
2021-01-04 17:28:43 +0100

    feat: move data to S3

--- src/pages/api/channels.ts
+++ src/pages/api/channels.ts
@@ -1,3 +1,6 @@
+import { S3 } from 'aws-sdk';
+import { Events } from 'aws-sdk/clients/cognitosync';
+import { StreamingEventStream } from 'aws-sdk/lib/event-stream/event-stream';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import TwitchApi from 'node-twitch';
 
@@ -6,74 +9,87 @@ const twitch = new TwitchApi({
   client_secret: process.env.TWITCH_CLIENT_SECRET,
 });
 
-const channelNames = [
-  'HasanAbi',
-  'IvySky',
-  'LillyVinnily',
-  'Natsumiii',
-  'physicalgamerz',
-  'PotasticP',
-  'Spaceboy',
-  'barrr_none',
-  'RuuTV',
-  'TheARTofKaidn',
-  'iamsimeonb',
-  'Seamoose',
-  'iBanjy',
-  'Its__Cole',
-  'MissPolaroid',
-  'IllKingKilla',
-  'Delt4Forc3',
-  'BarefootTasha',
-  'Z_H_O_R_A',
-  'Spiritendo',
-  'DuendePablo',
-  'NoTsch_',
-  'annelle',
-  'Nmplol',
-  'fps_shaka',
-  'Papaplatte',
-  'Lumenti',
-  'Arab',
-  'Trymacs',
-  'PoBr4tski',
-  'Kerem',
-  'thesmallpack',
-  'Muusoo',
-  'MonsterDface',
-  'JennyyL',
-  'juansguarnizo',
-  'Dekarldent',
-  'JulienBam',
-  'Perxitaa',
-  'rewinside',
-  'iBlali',
-  'alessandra',
-  'TherealJanHegenberg',
-  'SweeetTails',
-  'chairhandler',
-  'lisabby',
-  'TheGreatSkeletonMan',
-  'JargonTV',
-  'JACKZON',
-  'Dex5_',
-  'johndillon',
-  'roozworld',
-  'JamesDE',
-  'AikoBliss',
-  'AnniTheDuck',
-  'HerrNewstime',
-  'Stayi',
-  'Meloudya',
-  'missypwns',
-  'Baso',
-  'PainLivestream',
-  'emmadizzy',
-  'NebelNiek',
-  'Optimus_Slimed',
-];
+const isStreamingEventsStream = (
+  item: any
+): item is StreamingEventStream<Events> => typeof item.on === 'function';
 
+const queryChannels = <T = any>(expression: string) =>
+  new Promise<T[]>(async (resolve, reject) => {
+    const s3 = new S3({
+      region: process.env.S3_BUCKET_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET,
+      },
+    });
+    const { Payload } = await s3
+      .selectObjectContent({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: 'data/channels.csv',
+        ExpressionType: 'SQL',
+        Expression: expression,
+        InputSerialization: {
+          CSV: {
+            FileHeaderInfo: 'USE',
+            RecordDelimiter: '\n',
+            FieldDelimiter: ',',
+          },
+        },
+        OutputSerialization: {
+          JSON: {
+            RecordDelimiter: '\n',
+          },
+        },
+      })
+      .promise();
+
+    if (isStreamingEventsStream(Payload)) {
+      let data = '';
+      Payload.on('data', ({ Records }) => {
+        if (!Records) {
+          return;
+        }
+
+        data += Records.Payload.toString();
+      });
+      Payload.on('error', reject);
+      Payload.on('end', () => {
+        resolve(
+          data
+            .split('\n')
+            .map(record => {
+              if (!record.trim()) {
+                return null;
+              }
+              return JSON.parse(record);
+            })
+            .filter(a => a)
+        );
+      });
+    }
+  });
+
+const DEFAULT_LIMIT = 100;
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let limit = Number(req.query.limit || DEFAULT_LIMIT) || DEFAULT_LIMIT;
+  let offset = Number(req.query.offset || 0) || 0;
+
+  if (limit < 1 || limit > DEFAULT_LIMIT) {
+    limit = DEFAULT_LIMIT;
+  }
+
+  if (offset < 0) {
+    offset = 0;
+  }
+  const [{ _1: total }] = await queryChannels<{ _1: number }>(
+    `SELECT count(*) FROM S3Object s`
+  );
+  const queriedChannels = await queryChannels<{ channel: string }>(
+    `SELECT s.channel FROM S3Object s WHERE cast(s.row_index as Int) >= ${
+      offset + 1
+    } LIMIT ${limit}`
+  );
+  const channelNames = queriedChannels.map(({ channel }) => channel);
   const liveChannels = await twitch.getStreams({ channels: channelNames });
   const channels = [
     ...channelNames
@@ -103,5 +119,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     return 0;
   });
-  res.status(200).json({ channels });
+  res.status(200).json({ total, channels });
 };
